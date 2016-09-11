ef61b4a071c6984f9fd6a9571375bb287c66c174
Jmex <joan_ivan@hotmail.com>
2016-09-10 23:50:42 -0500

    Add files via upload

---
+++ pokedex_1/pokedex_1/scripts/main.js
@@ -0,0 +1,17 @@
+﻿$(document).on('click',".poke-sprite",function () {
+    $(this).fadeOut(500, function () {
+        if ($(this).attr("src").indexOf('-back') >= 0) {
+            $(this).attr("src", $(this).attr("src").replace('-back.gif', '.gif'));
+        }
+        else {
+            $(this).attr("src", $(this).attr("src").replace('.gif', '-back.gif'));
+        }
+    }).fadeIn(500);
+    return false;
+});
+
+$(document).on('click', ".toggle-icon", function () {
+    $(this).parent().toggleClass('unmarked');
+    $(this).parent().toggleClass('marked');
+    return false;
+});
